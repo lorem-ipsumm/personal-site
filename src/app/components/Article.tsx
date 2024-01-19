06d3e074851f16c837a49f39296c40eca99ed606
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2024-01-18 20:17:52 -0500

    fix: added indicator for copying code blocks

--- src/app/components/Article.tsx
+++ src/app/components/Article.tsx
@@ -6,13 +6,14 @@ import { useAtom } from "jotai";
 import { currentArticleAtom } from "../utils/atoms";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { Copy } from "react-feather";
+import { Check, Copy } from "react-feather";
 
 const Article = () => {
   // import atom
   const [currentArticle] = useAtom(currentArticleAtom);
   // text content
   const [content, setContent] = useState<string>("");
+  const [showCheck, setShowCheck] = useState<number>(-1);
 
   useEffect(() => {
     if (!currentArticle) return;
@@ -24,8 +25,13 @@ const Article = () => {
       });
   }, [currentArticle]);
 
-  const copyToClipboard = (text: string) => {
+  const copyToClipboard = (text: string, id: number) => {
+    // copy to clipboard
     navigator.clipboard.writeText(text);
+    // update state
+    setShowCheck(id);
+    // reset state after 1 second
+    setTimeout(() => setShowCheck(-1), 1000);
   };
 
   // syntax highlighting, code block styling, and line numbers
@@ -33,13 +39,21 @@ const Article = () => {
     code({ node, inline, className, children, ...props }: any) {
       const match = /language-(\w+)/.exec(className || "");
       const text = String(children).replace(/\n$/, "");
+      const id = node.position.end.line;
       return !inline && match ? (
         <Fragment>
-          <Copy
-            className="ease absolute right-4 top-4 cursor-pointer transition-all duration-500 hover:scale-90 hover:text-blue-500"
-            size={20}
-            onClick={() => copyToClipboard(text)}
-          />
+          {showCheck !== id ? (
+            <Copy
+              className="ease absolute right-4 top-4 cursor-pointer transition-all duration-500 hover:scale-90 hover:text-blue-500"
+              size={20}
+              onClick={() => copyToClipboard(text, id)}
+            />
+          ) : (
+            <Check
+              className="ease absolute right-4 top-4 cursor-pointer transition-all duration-500 stroke-green-500 animation-pulse"
+              size={20}
+            />
+          )}
           <SyntaxHighlighter
             style={solarizedlight}
             language={match[1]}
